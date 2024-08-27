913b7fd466efbc770c9a7b0fcaa781ecc205e4fb
Khalif Gaffarezka <kgaffarezka@gmail.com>
2024-08-27 10:57:56 +0700

    edit cors

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -1,9 +1,8 @@
 package routers
 
 import (
-	"time"
+	middleware "khalifgfrz/coffee-shop-be-go/internal/middlewares"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
 )
@@ -11,14 +10,21 @@ import (
 func New(db *sqlx.DB) *gin.Engine {
 	router := gin.Default()
 
-	router.Use(cors.New(cors.Config{
-        AllowAllOrigins:  true,
-        AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-        AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization", "application/json"},
-        ExposeHeaders:    []string{"Content-Length"},
-        AllowCredentials: true,
-        MaxAge:           12 * time.Hour,
-    }))
+	// config := cors.DefaultConfig()
+
+
+	// router.Use(cors.New(config))
+
+	// router.Use(cors.New(cors.Config{
+    //     AllowAllOrigins:  true,
+    //     AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+    //     AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization", "application/json"},
+    //     ExposeHeaders:    []string{"Content-Length"},
+    //     AllowCredentials: true,
+    //     MaxAge:           12 * time.Hour,
+    // }))
+
+	router.Use(middleware.CORSMiddleware())
 
 	authRouter(router, db)
 	orderRouter(router, db)
