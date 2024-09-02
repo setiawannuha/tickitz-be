b9b780ad7b0f0d7f4a0a2bd0bcca3300f575f03c
Khalif Gaffarezka <kgaffarezka@gmail.com>
2024-09-02 10:42:20 +0700

    edit server

--- pkg/server.go
+++ pkg/server.go
@@ -9,7 +9,7 @@ import (
 )
 
 func Server(router *gin.Engine) *http.Server {
-	var addr string = "0.0.0.0:8080"
+	var addr string = "0.0.0.0:8000"
 	if port := os.Getenv("PORT"); port != "" {
 		addr = ":" + port
 	}
